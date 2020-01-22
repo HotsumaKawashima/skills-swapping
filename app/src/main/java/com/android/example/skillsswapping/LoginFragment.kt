f1fd98a228ea6337b646c0f8a7b74b119c86834a
Yuka <yuka.0531.a@gmail.com>
2020-01-22 12:07:24 -0800

    Add onViewCreated & onClick methods (incomplete)

--- app/src/main/java/com/android/example/skillsswapping/LoginFragment.kt
+++ app/src/main/java/com/android/example/skillsswapping/LoginFragment.kt
@@ -6,7 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-class LoginFragment : Fragment() {
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+class LoginFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +20,18 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.signInBtn).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+       when(v!!.id){
+           R.id.signInBtn -> navController!!.navigate(R.id.action_loginFragment_to_topFragment)
+       }
     }
 
 
