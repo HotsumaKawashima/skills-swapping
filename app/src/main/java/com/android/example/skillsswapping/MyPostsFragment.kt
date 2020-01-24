ab7bd124c73c99dff072ad2419385b1b6fbe0755
Yuka <yuka.0531.a@gmail.com>
2020-01-23 16:26:48 -0800

    Add RecyclerView Adapter

--- app/src/main/java/com/android/example/skillsswapping/MyPostsFragment.kt
+++ app/src/main/java/com/android/example/skillsswapping/MyPostsFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.example.skillsswapping.domain.Post
+import com.android.example.skillsswapping.domain.User
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +20,30 @@ class MyPostsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // this is data for recycler view
+        val posts = arrayOf(
+            Post("post01post01post01post01post01post01post01"),
+            Post("post02post02post02post02post02post02post02"),
+            Post("post03post03post03post03post03post03post03"),
+            Post("post04post04post04post04post04post04post04"),
+            Post("post05post05post05post05post05post05post05"),
+            Post("post06post06post06post06post06post06post06")
+        )
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_posts, container, false)
+        val rootView:View = inflater.inflate(R.layout.fragment_my_posts, container, false)
+
+        // 1. get a reference to recyclerView
+        val recyclerView:RecyclerView = rootView.findViewById(R.id.myPostsRecyclerView)
+
+        // 2. set layoutManger
+        recyclerView.layoutManager = LinearLayoutManager(getActivity())
+
+        // 3. set adapter
+        recyclerView.adapter = MyPostsAdapter(getActivity(), posts)
+
+        return rootView
     }
 
 
