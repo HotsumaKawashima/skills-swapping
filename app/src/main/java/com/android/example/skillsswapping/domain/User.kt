f85906247fd95e29c0c368ff797353af84c0c932
Yuka <yuka.0531.a@gmail.com>
2020-01-24 15:23:16 -0800

    Add & Update Room database related files

--- app/src/main/java/com/android/example/skillsswapping/domain/User.kt
+++ app/src/main/java/com/android/example/skillsswapping/domain/User.kt
@@ -1,5 +1,16 @@
 package com.android.example.skillsswapping.domain
 
-class User {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-}
+@Entity(tableName = "users_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    var userid: Long,
+
+    @ColumnInfo(name = "user_name")
+    var username: String,
+
+    @ColumnInfo(name = "user_password")
+    var password: String)
